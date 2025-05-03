eba007365bf689ed7e47003b2112b375dde5821b
aarav1656 <anshuk3917@gmail..com>
2025-05-03 11:33:46 +0530

    route fix

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { Button } from './ui/button';
 import { Menu, X, ChevronDown } from 'lucide-react';
